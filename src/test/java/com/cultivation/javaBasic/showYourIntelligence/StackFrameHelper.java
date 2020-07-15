8bc343a724a10dc7adc1db6508896b9d0f3fb629
qinghua <qinghua.liu@thoughtworks.com>
2020-07-15 09:47:17 +0800

    practice analyzing stack trace elements

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/StackFrameHelper.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/StackFrameHelper.java
@@ -1,12 +1,13 @@
 package com.cultivation.javaBasic.showYourIntelligence;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class StackFrameHelper {
     public static String getCurrentMethodName() {
         // TODO: please modify the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        Throwable throwable = new Throwable();
+        StackTraceElement[] stackTrace = throwable.getStackTrace();
+        StackTraceElement lastStackTraceElement = stackTrace[1];
+        return lastStackTraceElement.getClassName() + "."+ lastStackTraceElement.getMethodName();
         // --end-->
     }
 }
